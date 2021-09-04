803dea24fa1bdaf9f08fe6deaa9b627e76be8e31
YEIMI_KATHERINE <arizayeimi28@gmail.com>
2021-09-04 15:56:01 -0500

    espero que si

--- js/script.js
+++ js/script.js
@@ -21,22 +21,3 @@ function main() {
         $(this).children(".children").slideToggle();
     });
 }
-var contador =1;
-
-function main() {
-    $(".bt-menu").click(function() {
-        if(contador ==1) {
-            $("nav").animate({
-                left: "-100%"
-            });
-            contador=0;
-        }
-        else {
-            $("nav").animate({
-                left: "0"
-
-            });
-            contador=1;
-        }
-    });
-}
