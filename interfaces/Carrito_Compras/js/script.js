67e2812dc756f6482b2932944992748f4b5fb970
Yeimi_peña <arizayeimi28@gmail.com>
2022-05-22 20:01:54 -0500

    cambios carritos

--- interfaces/Carrito_Compras/js/script.js
+++ interfaces/Carrito_Compras/js/script.js
@@ -1,6 +1,57 @@
 "use strict"
-function pruebaSuma(){
-    let cantidadLibro= document.getElementById("cantidadLibro").innerHTML;
-    cantidadLibro =Number(cantidadLibro) + 1;
-    document.getElementById("cantidadLibro").innerHTML = cantidadLibro;
+
+let valorUnidad= 25000;
+
+valorIndividual();
+calcularSubtotal();
+
+numeroProductos();
+
+function numeroProductos(){
+    let numeroProductos = document.getElementById("cantidadLibro").innerHTML;
+    document.getElementById("cantidad").innerHTML = numeroProductos;
+    if(numeroProductos >= 2 || numeroProductos == 0){
+        document.getElementById("cantidad").innerHTML= numeroProductos.toString()+ " productos";
+    }
+    else{
+        document.getElementById("cantidad").innerHTML= numeroProductos.toString()+ " producto";
+    }
 }
+
+
+function pruebaSuma(){
+    let cantidadArticulos= document.getElementById("cantidadLibro").innerHTML;
+    if (cantidadArticulos<=9) {
+        cantidadArticulos = Number(cantidadArticulos) + 1;
+        document.getElementById("cantidadLibro").innerHTML = cantidadArticulos;
+    }
+    else{
+        alert(" no puedes comprar mas de 10 productos en este pedido");
+    }
+    calcularSubtotal();
+    numeroProductos();
+
+}
+function pruebaResta(){
+    let cantidadArticulos= document.getElementById("cantidadLibro").innerHTML;
+    if (cantidadArticulos >0) {
+        cantidadArticulos =Number(cantidadArticulos) - 1;
+        document.getElementById("cantidadLibro").innerHTML = cantidadArticulos;
+    }
+    calcularSubtotal();
+    numeroProductos();
+}
+function calcularSubtotal() {
+    let cantidadElementos = document.getElementById("cantidadLibro").innerHTML;
+    let subtotal = valorUnidad * Number(cantidadElementos);
+    let subtotalFormat = new Intl.NumberFormat('es-ES').format(subtotal);
+    document.getElementById("valorSubtotal").innerHTML = subtotalFormat;
+}
+function valorIndividual() {
+    let valorUnidadFormat = new Intl.NumberFormat('es-ES').format(valorUnidad);
+    document.getElementById("valor-unitario").innerHTML = valorUnidadFormat,toString()+ "COP ";
+}
+
+
+
+
