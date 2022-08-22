7672f1679dd9ba7199977b1c0274bd6f36e89b3d
yeimi <arizayeimi28@gmail.com>
2022-08-21 23:41:51 -0500

    cambios producto interfaz

--- interfazPRODUCTO/script.js
+++ interfazPRODUCTO/script.js
@@ -15,3 +15,40 @@ salir.addEventListener("click",()=>{
 function intercambiarImagen(ruta){
     document.getElementById("ImgPrincipal").src= ruta;
 }
+
+
+//BOTONES SUMAR//
+
+let botonPlus=document.getElementById("plus");
+let botonMenos=document.getElementById("botonMenos");
+botonPlus.addEventListener("click",()=>{
+    sumar();
+})
+botonMenos.addEventListener("click",()=>{
+    
+    restar();
+})
+
+function sumar(){
+    let cantidad=document.getElementById("cantidad").innerHTML;
+    cantidad=parseInt(cantidad);
+    cantidad=cantidad +1;
+    
+    document.getElementById("cantidad").innerHTML=cantidad;
+    
+ let calcPrecio=220000 * cantidad;
+ document.getElementById("vcd").innerHTML="Costo Total : "+calcPrecio;
+}
+
+function restar(){
+    let cantidad=document.getElementById("cantidad").innerHTML;
+    if (cantidad>=1){
+    cantidad=parseInt(cantidad);
+    cantidad=cantidad -1;
+    
+    document.getElementById("cantidad").innerHTML=cantidad;
+
+    let calcPrecio=220000 * cantidad;
+    document.getElementById("vcd").innerHTML="Costo Total : "+calcPrecio;
+    }
+}
